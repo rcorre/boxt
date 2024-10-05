3080542ecf202fc8321db78f6232c2cd0e2acea9
Ryan Roden-Corrent <git@rrc.codes>
2024-10-05 17:00:52 -0400

    Draw a box.

---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+use std::io::Write;
+
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+
+type Canvas = Vec<Vec<String>>;
+
+#[derive(Serialize, Deserialize)]
+struct Rect {
+    x: usize,
+    y: usize,
+    w: usize,
+    h: usize,
+}
+
+impl Rect {
+    // ┌───┐
+    // │   │
+    // └───┘
+
+    pub fn draw(&self, canvas: &mut Canvas) {
+        if self.w == 0 || self.h == 0 {
+            log::warn!("Zero-area rect");
+            return;
+        }
+
+        const top_left: &str = "┌";
+        const top_right: &str = "┐";
+        const horizontal: &str = "─";
+        const vertical: &str = "│";
+        const bottom_left: &str = "└";
+        const bottom_right: &str = "┘";
+
+        let top = self.y;
+        let bottom = self.y + self.h - 1;
+        let left = self.x;
+        let right = self.x + self.w - 1;
+
+        for y in top..bottom {
+            canvas[y][left] = vertical.into();
+            canvas[y][right] = vertical.into();
+        }
+
+        for x in left..right {
+            canvas[top][x] = horizontal.into();
+            canvas[bottom][x] = horizontal.into();
+        }
+
+        canvas[top][left] = top_left.into();
+        canvas[top][right] = top_right.into();
+        canvas[bottom][left] = bottom_left.into();
+        canvas[bottom][right] = bottom_right.into();
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Document {
+    rect: Vec<Rect>,
+}
+
+impl Document {
+    fn max_xy(&self) -> (usize, usize) {
+        (
+            self.rect.iter().map(|r| r.x + r.w).max().unwrap_or(0),
+            self.rect.iter().map(|r| r.y + r.h).max().unwrap_or(0),
+        )
+    }
+
+    pub fn draw(&self, w: &mut impl Write) -> Result<()> {
+        let (max_x, max_y) = self.max_xy();
+        let mut res = vec![vec![" ".to_string(); max_x]; max_y];
+        for r in &self.rect {
+            r.draw(&mut res);
+        }
+
+        for row in res {
+            writeln!(w, "{}", row.join(""))?;
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, path::PathBuf};
+
+use anyhow::Result;
+use clap::{Args, Parser, Subcommand};
+use clart::Document;
+
+#[derive(Args)]
+struct RenderArgs {
+    path: PathBuf,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Render(RenderArgs),
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct CLI {
+    #[command(subcommand)]
+    command: Command,
+}
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    let args = CLI::parse();
+
+    match args.command {
+        Command::Render(args) => render(args),
+    }
+}
+
+fn render(args: RenderArgs) -> Result<()> {
+    let s = fs::read_to_string(args.path)?;
+    log::trace!("Parsing:\n{s}");
+    let doc: Document = toml::from_str(&s)?;
+    log::trace!("Parsing:\n{s}");
+    doc.draw(&mut std::io::stdout())?;
+    Ok(())
 }
